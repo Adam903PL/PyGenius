63fda0000aac2ef4b0e7c5c92273ec8b6932f5cd
Tracer903 <pukaluk.adam505@gmail.com>
2024-05-11 17:24:12 +0200

    update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/admin')
 def login():
-    return render_template('admin.html')
+    return render_template('admin.php')
 
 if __name__ == '__main__':
     app.run(debug=True)
