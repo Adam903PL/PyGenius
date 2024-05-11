2fb4e582b5b4aa0a020c4a91f63781f28342a34e
Tracer903 <pukaluk.adam505@gmail.com>
2024-05-11 18:24:34 +0200

    update 2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import hashlib
 
 app = Flask(__name__)
 
@@ -8,7 +9,21 @@ def index():
 
 @app.route('/admin')
 def login():
-    return render_template('admin.php')
+    return render_template('admin.html')
+
+
+
+@app.route('/login_data',methods=['POST'])
+def login_data():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        
+        with open("logs.txt", "a") as file:
+            file.write(f"Username: {username}, Password: {password}\n")
+
+        
+        return f'Login attempt with username: {username} and password: {password}'
 
 if __name__ == '__main__':
     app.run(debug=True)
